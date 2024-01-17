4131779c1e51310f08ee575a162f7a93592a83ce
Vark 1623 <vark.is.life@gmail.com>
2024-01-17 17:38:14 +0530

    Add files via upload

---
+++ components/MediaItem.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import useLoadImage from "@/hooks/useLoadImage";
+import { Song } from "@/types";
+import Image from "next/image";
+
+interface MediaItemProps {
+    data: Song;
+    onClick?: (id: string) => void;
+}
+
+const MediaItem: React.FC<MediaItemProps> = ({
+    data,
+    onClick
+}) => {
+    const imageUrl = useLoadImage(data);
+
+    const handleClick = () => {
+        if (onClick) {
+            return onClick(data.id);
+        }
+    }
+    return (
+        <div
+            onClick={handleClick}
+            className="
+                flex
+                items-center
+                gap-x-3
+                cursor-pointer
+                hover:bg-neutral-800/50w-full
+                p-1
+                rounded-md
+            "
+        >
+            <div
+                className="
+                    relative
+                    rounded-md
+                    min-h-[48px]
+                    min-w-[48px]
+                    overflow-hidden
+                "
+            >
+                <Image
+                   fill
+                   src={imageUrl || '/images/liked.png'}
+                   alt="Media Item"
+                   className="object-cover" 
+                />
+            </div>
+            <div className="
+                flex
+                flex-col
+                gap-y-1
+                overflow-hidden
+            ">
+                <p className="text-white font-semibold truncate">
+                    {data.title}
+                </p>
+                <p className="text-neutral-400 text-sm font-medium truncate">
+                    {data.author}
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default MediaItem;
